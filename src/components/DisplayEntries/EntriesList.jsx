78951adffe09a9e752dd3ed1032a8b1812a5f4b1
KDSigel <heykarl@gmail.com>
2021-12-15 08:54:49 -0800

    still with node yml file

--- src/components/DisplayEntries/EntriesList.jsx
+++ src/components/DisplayEntries/EntriesList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useEntry } from '../../context/EntryContext'
 import EntryDetail from '../Entries/EntryDetail'
-import './EntriesList.css'
 
 export default function EntriesList() {
 
